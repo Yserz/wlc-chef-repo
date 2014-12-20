7bc8d0bbde171e8eede37dafbdb85b57a20cb9cd
vagrant <michael.koppen@googlemail.com>
2014-12-20 17:31:06 +0000

    removed wlc nano cookbook

--- cookbooks/wlc-nano/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'wlc-nano'
-maintainer       'Michael Koppen'
-maintainer_email 'michael.koppen@welovecoding.com'
-license          'All rights reserved'
-description      'Installs/Configures wlc-nano'
-long_description 'Installs/Configures wlc-nano'
-version          '0.1.0'
-
-depends 'nano', '~> 1.0.0'
--- cookbooks/wlc-nano/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: wlc-nano
-# Recipe:: default
-#
-# Copyright (C) 2014 Michael Koppen
-#
-# All rights reserved - Do Not Redistribute
-#
-include_recipe 'nano'
