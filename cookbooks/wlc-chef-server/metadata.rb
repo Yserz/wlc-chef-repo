7ec8b8fad1e5ebfdf25fc7921fc4853b186f6b4a
vagrant <michael.koppen@googlemail.com>
2014-12-20 15:16:42 +0000

    added wlc-chef-server application cookbook

---
+++ cookbooks/wlc-chef-server/metadata.rb
@@ -0,0 +1,8 @@
+name             'wlc-chef-server'
+maintainer       'Michael Koppen'
+maintainer_email 'michael.koppen@welovecoing.com'
+license          'All rights reserved'
+description      'Installs/Configures wlc-chef-server'
+long_description 'Installs/Configures wlc-chef-server'
+version          '0.1.0'
+depends 'chef-server', '~> 2.1.6'
---
+++ cookbooks/wlc-chef-server/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: wlc-chef-server
+# Recipe:: default
+#
+# Copyright (C) 2014 Michael Koppen
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'chef-server'
